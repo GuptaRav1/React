85a2f885036e1581aaf35741dd5d3db765e19da9
Ravi Gupta <ravigupta3282@gmail.com>
2024-08-11 12:35:44 +0530

    added coverage in gitignore

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -10,3 +10,30 @@ test('Should render contact page in browser.', () => {
   expect(heading).toBeInTheDocument()
 
 })
+
+test('Should render button in contact.', () => {
+  render(<Contact />)
+
+  const button = screen.getByRole('button')
+
+  expect(button).toBeInTheDocument()
+
+})
+
+test('Should render input name inside contact.', () => {
+  render(<Contact />)
+
+  const inputName = screen.getByPlaceholderText('name')
+
+  expect(inputName).toBeInTheDocument()
+
+})
+
+test('Should load all input boxes', () => {
+  render(<Contact />)
+
+  const inputBoxes = screen.getAllByRole('textbox')
+
+  expect(inputBoxes.length).toBe(2)
+
+})
